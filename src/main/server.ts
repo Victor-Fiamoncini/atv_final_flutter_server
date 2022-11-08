afc05e06a57545cd1d79d8f1c4e161880f173f4b
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2022-11-07 22:39:04 -0300

    fix: tsconfig path with eslint

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,15 +1,21 @@
 import express from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
 
 const initApp = async () => {
   try {
     const env = { port: 3333 }
     const app = express()
 
+    app.use(helmet())
+    app.use(express.json())
+    app.use(morgan('dev'))
+
     app.get('/', (req, res) => res.json('helloooooooooo'))
 
     app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
   } catch (err) {
-    console.error(err)
+    console.log(err)
   }
 }
 
