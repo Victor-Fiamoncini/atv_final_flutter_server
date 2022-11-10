ef7a41e66d36f8d4c69001c70fc1fb21b6e3b7dd
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2022-11-10 20:55:24 -0300

    refactor: make all fields nullable in RegisterWeatherWithAddressValidator

--- src/validation/validators/RegisterWeatherWithAddressValidator.ts
+++ src/validation/validators/RegisterWeatherWithAddressValidator.ts
@@ -5,20 +5,20 @@ import * as Yup from 'yup'
 export class RegisterWeatherWithAddressValidator implements ObjectValidator {
   private readonly validationSchema = Yup.object().shape({
     address: Yup.object().shape({
-      street: Yup.string().required('Address street is required').typeError('Invalid street'),
-      neighborhood: Yup.string().required('Address neighborhood is required').typeError('Invalid neighborhood'),
-      city: Yup.string().required('Address city is required').typeError('Invalid city'),
-      state: Yup.string().required('Address state is required').typeError('Invalid state'),
-      country: Yup.string().required('Address country is required').typeError('Invalid country'),
-      postalCode: Yup.string().required('Address postal code is required').typeError('Invalid postal code'),
+      street: Yup.string().nullable().typeError('Invalid street'),
+      neighborhood: Yup.string().nullable().typeError('Invalid neighborhood'),
+      city: Yup.string().nullable().typeError('Invalid city'),
+      state: Yup.string().nullable().typeError('Invalid state'),
+      country: Yup.string().nullable().typeError('Invalid country'),
+      postalCode: Yup.string().nullable().typeError('Invalid postal code'),
     }),
     weather: Yup.object().shape({
-      maxTemperature: Yup.number().required('Weather max temperature is required').typeError('Invalid max temperature'),
-      minTemperature: Yup.number().required('Weather min temperature is required').typeError('Invalid min temperature'),
-      mainTemperature: Yup.number().required('Weather temperature is required').typeError('Invalid temperature'),
-      humidity: Yup.number().required('Weather humidity is required').typeError('Invalid humidity'),
-      clouds: Yup.number().required('Weather clouds is required').typeError('Invalid clouds'),
-      windSpeed: Yup.number().required('Weather wind speed is required').typeError('Invalid wind speed'),
+      maxTemperature: Yup.number().nullable().typeError('Invalid max temperature'),
+      minTemperature: Yup.number().nullable().typeError('Invalid min temperature'),
+      mainTemperature: Yup.number().nullable().typeError('Invalid temperature'),
+      humidity: Yup.number().nullable().typeError('Invalid humidity'),
+      clouds: Yup.number().nullable().typeError('Invalid clouds'),
+      windSpeed: Yup.number().nullable().typeError('Invalid wind speed'),
     }),
   })
 
